ebed57b6a679cb49448e8370225d021b0e427ec5
Maximus <maks.shherbakov04@gmail.com>
2024-06-25 21:05:32 +0200

    Preparation for the 1st homework from the second course

--- homework01/fizzbuzz/fizzbuzz.go
+++ homework01/fizzbuzz/fizzbuzz.go
@@ -1,20 +1,6 @@
 package fizzbuzz
 
-func FizzBuzz(i int) (string, int) {
-
-	switch {
-	case i%15 == 0:
-
-		return "FizzBuzz", i
-
-	case i%3 == 0:
-
-		return "Fizz", i
-
-	case i%5 == 0:
-
-		return "Buzz", i
-	}
-
-	return "", i
+func FizzBuzz(i int) string {
+	// TODO
+	return ""
 }
