4f2703c5b82bbac1f25b56973b7215a99613afa2
Maximus <maks.shherbakov04@gmail.com>
2024-06-25 21:05:32 +0200

    Preparation for the 1st homework from the second course

--- homework01/fizzbuzz/fizzbuzz.go
+++ homework01/fizzbuzz/fizzbuzz.go
@@ -1,25 +1,6 @@
 package fizzbuzz
 
-import "fmt"
-
 func FizzBuzz(i int) string {
-
-	switch {
-	case i%15 == 0:
-
-		return "FizzBuzz"
-
-	case i%3 == 0:
-
-		return "Fizz"
-
-	case i%5 == 0:
-
-		return "Buzz"
-
-	default:
-
-		fmt.Println(i)
-	}
+	// TODO
 	return ""
 }
