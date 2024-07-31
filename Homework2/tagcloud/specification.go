7cd13b5e94bc012a88558dfda056157837dadbe1
Maksim Shcherbakov <94864283+M4RFF@users.noreply.github.com>
2024-07-31 13:14:55 +0200

    [Hw2] Implemented a structure with the semantics of a tag cloud.

--- Homework2/tagcloud/specification.go
+++ Homework2/tagcloud/specification.go
@@ -1,8 +1,13 @@
 package tagcloud
 
+import (
+	"sort"
+	"strings"
+)
+
 // TagCloud aggregates statistics about used tags
 type TagCloud struct {
-	tags map[string]int
+	tags map[string]int // cloud of tags
 }
 
 // TagStat represents statistics regarding single tag
@@ -12,22 +17,38 @@ type TagStat struct {
 }
 
 // New should create a valid TagCloud instance
+// Pointer returns
 func New() *TagCloud {
-	return &TagCloud{tags: make(map[string]int)}
+	return &TagCloud{make(map[string]int)} // creat a new cloud of tags
 }
 
 // AddTag should add a tag to the cloud if it wasn't present and increase tag occurrence count
 // thread-safety is not needed
-func (tagCloud *TagCloud) AddTag(tag string) {
-	tagCloud.tags[tag]++
+func (tgs *TagCloud) AddTag(tag string) {
+	tag = strings.ToLower(tag) // add tag to a cloud
+	tgs.tags[tag]++            // increase tag occurrence count
 }
 
 // TopN should return top N most frequent tags ordered in descending order by occurrence count
-// if there are multiple tags with the same occurrence count then the order is defined by implementation
+// if there are multiple TAGS with the same occurrence COUNT then the order is defined by implementation
 // if n is greater that TagCloud size then all elements should be returned
 // thread-safety is not needed
 // there are no restrictions on time complexity
-func (tagCloud *TagCloud) TopN(n int) []TagStat {
+func (tgs *TagCloud) TopN(n int) []TagStat {
+	tagStats := make([]TagStat, 0, len(tgs.tags)) // create tagStats which collects tags
 
-	return nil
+	for tag, count := range tgs.tags {
+		tagStats = append(tagStats, TagStat{tag, count}) // add each Tag and his Count
+	}
+
+	// sort by descending frequency of tag use
+	sort.Slice(tagStats, func(i, j int) bool {
+		return tagStats[i].OccurrenceCount > tagStats[j].OccurrenceCount
+	})
+
+	if n > len(tagStats) { // if n greater than number of tags
+		return tagStats // i return all elements from tagStats
+	}
+
+	return tagStats[:n] // return first n elements from tagStats
 }
